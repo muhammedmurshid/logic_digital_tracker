84b5e4c1136e385385f68f1d94676d5e443ae64b
argenspin <argenspin@gmail.com>
2023-09-25 16:53:13 +0530

    changed order of kanban view to desc order by deadline

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'author': 'Rizwaan',
     'version': "14.0.1.0",
     'sequence': "0",
-    'depends': ['base','mail'],
+    'depends': ['base','mail','project'],
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
